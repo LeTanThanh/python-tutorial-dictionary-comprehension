af8c31e93a82b1732051ce33776bdbe02dafbdab
TanThanhLe <tanthanhcr95@gmail.com>
2024-05-01 13:42:18 +0700

    Using Python dictionary comprehension to filter a dictionary

--- main.py
+++ main.py
@@ -34,3 +34,34 @@ if __name__ == "__main__":
 
   new_stocks = { symbol: price * 1.02 for (symbol, price) in stocks.items() }
   print(new_stocks)
+
+  # Using Python dictionary comprehension to filter a dictionary
+
+  stocks = {
+    "AAPL": 121,
+    "AMZN": 3380,
+    "MSFT": 219,
+    "BIIB": 280,
+    "QDEL": 266,
+    "LVGO": 144
+  }
+  print(stocks)
+
+  selected_stocks = {}
+  for symbol, price in stocks.items():
+    if price > 200:
+      selected_stocks[symbol] = price
+  print(selected_stocks)
+
+  stocks = {
+    "AAPL": 121,
+    "AMZN": 3380,
+    "MSFT": 219,
+    "BIIB": 280,
+    "QDEL": 266,
+    "LVGO": 144
+  }
+  print(stocks)
+
+  selected_stocks = { symbol: price for (symbol, price) in stocks.items() if price > 200 }
+  print(selected_stocks)
